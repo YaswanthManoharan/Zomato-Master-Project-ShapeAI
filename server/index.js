555d3430062da1a9dd26fdced6befa831a20a72a
YaswanthManoharan <yaswanthtarun1@gmail.com>
2021-09-07 18:16:35 +0530

    server setup

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+const zomato = express();
+
+//application middlewares
+zomato.use(helmet());
+zomato.use(cors());
+
+zomato.get("/",(req,res)=>res.json({message:"Setup success"}));
+
+zomato.listen(4000,()=>console.log.apply("Server is running"));
