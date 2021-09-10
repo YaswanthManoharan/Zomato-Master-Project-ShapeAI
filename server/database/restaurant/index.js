e9c919e623001c125f0274c66597a920a96dfef6
YaswanthManoharan <yaswanthtarun1@gmail.com>
2021-09-10 12:10:59 +0530

    add statics and methods, refactor signup route

--- server/database/restaurant/index.js
+++ server/database/restaurant/index.js
@@ -8,7 +8,7 @@ const RestaurantSchema=new mongoose.Schem({
     cuisine:[String],
     restaurantTimings:String,
     contactNumber:Number,
-    website:Number,
+    website:String,
     popularDishes:[String],
     averageCost:Number,
     amenties:[String],
--- server/index.js
+++ server/index.js
@@ -1,14 +1,30 @@
+//importing env variables
+require("dotenv").config();
+//libraries
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-const zomato = express();
+import mongoose from "mongoose";
+//microservice routes
 
+import Auth from "./API/Auth";
+
+//database connection
+import ConnectDB from "./database/connection";
+const zomato = express();
 //application middlewares
 zomato.use(helmet());
 zomato.use(cors());
 zomato.use(express.json());
 zomato.use(express.urlencoded({extended:false}));
 
+//App routes
+
+zomato.use("/auth",Auth);
 zomato.get("/",(req,res)=>res.json({message:"Setup success"}));
 
-zomato.listen(4000,()=>console.log.apply("Server is running"));
+ //zomato.listen(4000,()=>ConnectDB().then(()=>console.log("Server is running")).catch((error)=>console.log(error)));
+const port = process.env.PORT || 4000;
+ zomato.listen(port,()=>ConnectDB().then(()=>console.log("Server is running")).catch(()=>console.log("Server is running but connection with db is not established")));
+
+//zomato.listen(4000,console.log("Server is running"));
